e8df97f27f447fcfa0809ab375f1b36a5e091699
michael <michaeltilman77@gmail.com>
2018-06-05 17:18:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(firstNum, secondNum){
+  return firstNum+secondNum
+}
+function subtract(firstNum,secondNum){
+  return secondNum - firstNum
+}
+function multiply(firstNum,secondNum){
+  return firstNum*secondNum
+}
+function inc(number){
+  return ++number
+}
+function dec(number){
+  return --number
+}
+function makeInt(string){
+  parseInt(float, 10)
+}
+function preserveDecimal()
